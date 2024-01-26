dfd57aa2c44045bf3c2fb3870c0e7eb3f65ca7d3
arm-out <arminsuraj@gmail.com>
2024-01-26 10:06:32 -0800

    post submission

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,77 @@
 <script lang="ts">
-	import moment from 'moment-timezone';
+	import moment, { type Moment } from 'moment-timezone';
 	import { onMount } from 'svelte';
+	import Modal from './Modal.svelte';
+
+	type Post = null | {
+		image: string;
+		caption: string;
+		time: string;
+	};
 
 	let armTime = moment.utc().tz('America/Los_Angeles').format('ddd, DD MMM h:mm A');
-	let rakTime = moment().tz('Europe/Dublin').format('ddd, DD MMM h:mm A');
+	let rakTime = moment.utc().tz('Europe/Dublin').format('ddd, DD MMM h:mm A');
+
+	let armStreak = 0;
+	let rakStreak = 0;
+
+	$: console.log(showModal);
+
+	let reader: FileReader;
+	let armInput: HTMLInputElement;
+	let rakInput: HTMLInputElement;
+	let preview: HTMLImageElement;
+	let showModal = false;
+	let currUser = '';
+	let caption = '';
+
+	let armPost: Post = null;
+	let rakPost: Post = null;
+
+	function onChange(user: string) {
+		currUser = user;
+		let file = user === 'arm' ? armInput.files![0] : rakInput.files![0];
+
+		if (file) {
+			reader.addEventListener('load', function () {
+				preview.setAttribute('src', reader.result as string);
+			});
+			reader.readAsDataURL(file);
+			showModal = true;
+			return;
+		}
+	}
+
+	function handlePost(e: CustomEvent) {
+		switch (e.detail.user) {
+			case 'arm':
+				armPost = {
+					image: reader.result as string,
+					caption: e.detail.caption,
+					time: moment.utc().tz('America/Los_Angeles').format('ddd, DD MMM h:mm A')
+				};
+				armStreak++;
+				break;
+			case 'rak':
+				rakPost = {
+					image: reader.result as string,
+					caption: e.detail.caption,
+					time: moment.utc().tz('Europe/Dublin').format('ddd, DD MMM h:mm A')
+				};
+				rakStreak++;
+				break;
+		}
+
+		showModal = false;
+		caption = '';
+	}
 
 	onMount(() => {
+		reader = new FileReader();
+
 		const interval = setInterval(() => {
 			armTime = moment.utc().tz('America/Los_Angeles').format('ddd, DD MMM h:mm A');
-			rakTime = moment().tz('Europe/Dublin').format('ddd, DD MMM h:mm A');
+			rakTime = moment.utc().tz('Europe/Dublin').format('ddd, DD MMM h:mm A');
 		}, 1000);
 
 		return () => {
@@ -18,24 +81,75 @@
 </script>
 
 <main class="h-[100dvh] flex flex-col max-w-[35rem] p-2 gap-2">
-	<section class="bg-red-400 h-full flex flex-col justify-between rounded-xl p-4">
+	<section
+		style={armPost
+			? `background-image: url(${armPost.image})`
+			: 'background-color: rgb(248 113 113)'}
+		class="h-full flex flex-col justify-between rounded-xl p-4 bg-cover {armPost
+			? 'text-white'
+			: ''}"
+	>
 		<div class="flex flex-col">
-			<h2 class="text-4xl">arm</h2>
-			<h3>{armTime}</h3>
+			<h2 class="text-4xl font-semibold">arm</h2>
+			<h3>{armPost ? armPost.time : armTime}</h3>
 		</div>
-		<div class="flex flex-row justify-between">
-			<p>Streak: 🔥</p>
-			<button>Add</button>
+		<div class="flex flex-row justify-between items-center">
+			<p class="font-semibold text-3xl min-w-[5rem]">{armStreak}🔥</p>
+			{#if armPost}
+				<p class="italic">{armPost.caption}</p>
+			{:else}
+				<label class="cursor-pointer bg-white py-1 px-4 rounded-full">
+					<input
+						bind:this={armInput}
+						on:change={() => onChange('arm')}
+						type="file"
+						class="hidden"
+					/>
+					Update
+				</label>
+			{/if}
 		</div>
 	</section>
-	<section class="bg-indigo-400 h-full flex flex-col justify-between rounded-xl p-4">
+	<section
+		style={rakPost
+			? `background-image: url(${rakPost.image})`
+			: 'background-color: rgb(129 140 248)'}
+		class="h-full flex flex-col justify-between rounded-xl p-4 bg-cover bg-center {rakPost
+			? 'text-white'
+			: ''}"
+	>
 		<div class="flex flex-col">
-			<h2 class="text-4xl">rak</h2>
-			<h3>{rakTime}</h3>
+			<h2 class="text-4xl font-semibold">rak</h2>
+			<h3>{rakPost ? rakPost.time : rakTime}</h3>
 		</div>
-		<div class="flex flex-row justify-between">
-			<p>Streak: 🔥</p>
-			<button>Add</button>
+		<div class="flex flex-row justify-between items-center">
+			<p class="font-semibold text-3xl min-w-[5rem]">{rakStreak}🔥</p>
+			{#if rakPost}
+				<p class="italic">{rakPost.caption}</p>
+			{:else}
+				<label class="cursor-pointer bg-white py-1 px-4 rounded-full">
+					<input
+						bind:this={rakInput}
+						on:change={() => onChange('rak')}
+						type="file"
+						class="hidden"
+					/>
+					Update
+				</label>
+			{/if}
 		</div>
 	</section>
 </main>
+
+<Modal bind:showModal on:post={handlePost} user={currUser} {caption}>
+	<div>
+		<h2 class="text-2xl font-bold">Upload Image</h2>
+		<img bind:this={preview} src="" alt="Preview" class="py-2" />
+		<input
+			bind:value={caption}
+			type="text"
+			placeholder="Caption"
+			class="border-b-2 w-full focus:outline-none"
+		/>
+	</div>
+</Modal>
---
+++ src/routes/Modal.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let showModal: boolean;
+	export let user: string;
+	export let caption: string;
+
+	let dialog: HTMLDialogElement;
+
+	$: if (dialog && showModal) dialog.showModal();
+
+	const dispatch = createEventDispatcher();
+
+	function submit() {
+		dispatch('post', { user, caption });
+		dialog.close();
+	}
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={() => dialog.close()}
+>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div on:click|stopPropagation>
+		<slot />
+		<div class="pt-4 flex flex-row gap-2">
+			<button on:click={submit} class="border border-black rounded text-white bg-black p-1"
+				>Submit</button
+			>
+			<button on:click={() => dialog.close()} class="border border-red-400 rounded text-red-500 p-1"
+				>Cancel</button
+			>
+		</div>
+	</div>
+</dialog>
+
+<style>
+	dialog {
+		max-width: 90vw;
+		border-radius: 0.2em;
+		border: none;
+		padding: 0;
+	}
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.3);
+	}
+	dialog > div {
+		padding: 1em;
+	}
+	dialog[open] {
+		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+	@keyframes zoom {
+		from {
+			transform: scale(0.95);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
+	dialog[open]::backdrop {
+		animation: fade 0.2s ease-out;
+	}
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+	button {
+		display: block;
+	}
+</style>
